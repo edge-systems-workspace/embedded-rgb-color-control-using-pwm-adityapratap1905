48181985269482375c89f5f014f888da166e05e4
adityapratap1905 <adityapratap1905@gmail.com>
2026-02-27 16:31:23 +0530

    add RGB LED setup function and define LED pin

--- src/main.cpp
+++ src/main.cpp
@@ -2,12 +2,17 @@
 /**
  * @file main.cpp
  * @brief Embedded RGB LED Control (Digital + PWM)
- * @author YOUR_NAME
- * @date YYYY-MM-DD
+ * @author himanshu gupta
+ * @date 2026-02-20
  *
  * @details
  * Controls RGB LED using digital ON/OFF
  * and analog PWM brightness control.
  */
 
+int led_pin=2;
+
+void setup() {
+    pinMode(led_pin, OUTPUT);
+}
 
